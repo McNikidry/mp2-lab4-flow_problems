351204a55543ad7dbd6620505867a208a8db1e7d
Nikita <nikidry70@gmail.com>
2016-11-28 19:13:05 +0300

    Первая реализация Лабораторной работы

---
+++ Соловьев/base/sample_system.cpp
@@ -0,0 +1,26 @@
+﻿#include "system.h"
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	unsigned int n;
+
+	cout << " Введите количество тактов Tack = ";
+	cin >> n;
+	cout << endl;
+
+	double a, b;
+
+	cout << "Введите приоритет появления новой задачи в пределах от 0 до 1, A = ";
+	cin >> a;
+	cout << endl;
+	cout << "Введите приоритет выполнения задачи в пределах от 0 до 0.9, B = ";
+	cin >> b;
+	cout << endl;
+
+	Task sample(n, a, b);
+
+	sample.WorkWithTasks();
+
+	return 0;
+}
